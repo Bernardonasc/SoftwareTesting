62172d26fd7eb7c0673a5ce83461a487cff2baae
samuel-brum <samuelbrummartins@gmail.com>
2024-05-23 18:20:22 -0300

    adapta testes para pytest

--- myproject/tests/test_models.py
+++ myproject/tests/test_models.py
@@ -1,19 +1,18 @@
-from django.test import TestCase
+import pytest
 from django.contrib.auth.models import User
 
-class TestModels(TestCase):
+@pytest.fixture
+def user1(db):
+    return User.objects.create_user(username='testuser1', password='12345')
 
-    def setUp(self):
-        self.user1 = User.objects.create_user(username='testuser1', password='12345')
+def test_user_creation(user1):
+    assert user1.username == 'testuser1'
 
-    def test_user_creation(self):
-        self.assertEquals(self.user1.username, 'testuser1')
+def test_default_user_is_active(user1):
+    assert user1.is_active == True
 
-    def test_default_user_is_active(self):
-        self.assertEquals(self.user1.is_active, True)
+def test_default_user_is_not_staff(user1):
+    assert user1.is_staff == False
 
-    def test_default_user_is_not_staff(self):
-        self.assertEquals(self.user1.is_staff, False)
-
-    def test_default_user_is_not_superuser(self):
-        self.assertEquals(self.user1.is_superuser, False)
+def test_default_user_is_not_superuser(user1):
+    assert user1.is_superuser == False
--- myproject/tests/test_views.py
+++ myproject/tests/test_views.py
@@ -1,69 +1,56 @@
-from django.test import TestCase, Client
+import pytest
 from django.urls import reverse
+from django.test import Client
 from django.contrib.auth.models import User
 from blog.models import Post, Category
 
-class TestViews(TestCase):
+@pytest.fixture
+def client(db):
+    return Client()
 
-    def setUp(self):
-        self.client = Client()
-        self.user = User.objects.create_user(username='testuser', password='12345')
-        self.category = Category.objects.create(name='category1')
-        self.post = Post.objects.create(title='title1', content='content1', category=self.category, author=self.user)
-        self.home_url = reverse('home')
-        self.posts_by_category_url = reverse('posts_by_category', args=[self.category.id])
-        self.register_url = reverse('register')
-        self.post_detail_url = reverse('post_detail', args=[self.post.id])
-        self.login_url = reverse('login')
-        self.logout_url = reverse('logout')
-        self.post_create_url = reverse('post_create')
-        self.post_edit_url = reverse('post_edit', args=[self.post.id])
-        self.post_delete_url = reverse('post_delete', args=[self.post.id])
+@pytest.fixture
+def user(transactional_db):
+    return User.objects.create_user(username='testuser', password='12345')
 
-    def test_home_GET(self):
-        response = self.client.get(self.home_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
+@pytest.fixture
+def category(transactional_db):
+    return Category.objects.create(name='TestCategory')
 
-    def test_posts_by_category_GET(self):
-        response = self.client.get(self.posts_by_category_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'posts_by_category.html')
+@pytest.fixture
+def post(transactional_db, user, category):
+    return Post.objects.create(title='TestPost', content='TestContent', author=user, category=category)
 
-    def test_register_GET(self):
-        response = self.client.get(self.register_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'register.html')
+def test_home_GET(client):
+    response = client.get(reverse('home'))
+    assert response.status_code == 200
 
-    def test_post_detail_GET(self):
-        response = self.client.get(self.post_detail_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'post_detail.html')
+def test_posts_by_category_GET(client, category):
+    response = client.get(reverse('posts_by_category', args=[category.id]))
+    assert response.status_code == 200
 
-    def test_login_GET(self):
-        response = self.client.get(self.login_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'login.html')
+def test_register_GET(client):
+    response = client.get(reverse('register'))
+    assert response.status_code == 200
 
-    def test_logout_GET(self):
-        self.client.login(username='testuser', password='12345')
-        response = self.client.get(self.logout_url)
-        self.assertEquals(response.status_code, 302)
+def test_post_detail_GET(client, post):
+    response = client.get(reverse('post_detail', args=[post.id]))
+    assert response.status_code == 200
 
-    def test_post_create_GET(self):
-        self.client.login(username='testuser', password='12345')
-        response = self.client.get(self.post_create_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'post_form.html')
+def test_loginPage_GET(client):
+    response = client.get(reverse('login'))
+    assert response.status_code == 200
 
-    def test_post_edit_GET(self):
-        self.client.login(username='testuser', password='12345')
-        response = self.client.get(self.post_edit_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'post_form.html')
+def test_post_create_GET(client, user):
+    client.login(username='testuser', password='12345')
+    response = client.get(reverse('post_create'))
+    assert response.status_code == 200
 
-    def test_post_delete_GET(self):
-        self.client.login(username='testuser', password='12345')
-        response = self.client.get(self.post_delete_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'post_confirm_delete.html')
+def test_post_edit_GET(client, user, post):
+    client.login(username='testuser', password='12345')
+    response = client.get(reverse('post_edit', args=[post.id]))
+    assert response.status_code == 200
+
+def test_post_delete_GET(client, user, post):
+    client.login(username='testuser', password='12345')
+    response = client.get(reverse('post_delete', args=[post.id]))
+    assert response.status_code == 200
